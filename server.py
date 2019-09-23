3f37f969b20edb36c17081d0432f1b2aef4f8f0c
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2019-09-23 13:20:11 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,13 +1,18 @@
 import socket
 import time
 from multiprocessing import Process
-from concurrent.futures import ThreadPoolExecutor
+from threading import Thread
 
 
 class Server(Process):
-    def __init__(self, port):
+    def __init__(self, port, n=3):
         super().__init__()
         self.port = port
+        self.n = n
+
+    def processes(self):
+        for i in range(self.n):
+            Process(target=self.connect(), args=())
 
     def connect(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
@@ -16,44 +21,35 @@ class Server(Process):
             while True:
                 conn, addr = sock.accept()
                 conn.settimeout(15)
-                with conn:
-                    while True:
-                        try:
-                            data = conn.recv(1024)
-                            if not data:
-                                break
-                            comm = data.decode('utf-8')
-                            t = time.ctime(time.time())
-                            if comm == 'hour':
-                                conn.sendall(t[-13:t.find(':')].encode('utf-8'))
-                            elif comm == 'minutes':
-                                conn.sendall(t[t.find(':')+1:t.rfind(':')].encode('utf-8'))
-                            elif comm == 'seconds':
-                                conn.sendall(t[t.rfind(':')+1:t.rfind(' ')].encode('utf-8'))
-                            elif comm == 'stop':
-                                conn.sendall('Connection has been closed'.encode('utf-8'))
-                                break
-                            else:
-                                    conn.sendall('ERROR'.encode('utf-8'))
-                        except socket.timeout:
-                            conn.sendall('Connection has been closed by timeout'.encode('utf-8'))
-                            break
-                        except socket.error as err:
-                            print('Error', err)
-                            break
+                Thread(target=self.worker(conn))
 
-    def work(self):
-        with ThreadPoolExecutor(max_workers=5) as pool:
-            pool.submit(Server(self.port).connect())
+    def worker(self, conn):
+        with conn:
+            while True:
+                try:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    comm = data.decode('utf-8')
+                    t = time.ctime(time.time())
+                    if comm == 'hour':
+                        conn.sendall(t[-13:t.find(':')].encode('utf-8'))
+                    elif comm == 'minutes':
+                        conn.sendall(t[t.find(':') + 1:t.rfind(':')].encode('utf-8'))
+                    elif comm == 'seconds':
+                        conn.sendall(t[t.rfind(':') + 1:t.rfind(' ')].encode('utf-8'))
+                    elif comm == 'stop':
+                        conn.sendall('Connection has been closed'.encode('utf-8'))
+                        break
+                    else:
+                        conn.sendall('ERROR'.encode('utf-8'))
+                except socket.timeout:
+                    conn.sendall('Connection has been closed by timeout'.encode('utf-8'))
+                    break
+                except socket.error as err:
+                    print('Error', err)
+                    break
 
 
 if __name__ == '__main__':
-    p1 = Process(target=Server(10000).work, args=())
-    p2 = Process(target=Server(10001).work, args=())
-    p3 = Process(target=Server(10002).work, args=())
-    p1.start()
-    p2.start()
-    p3.start()
-    p1.join()
-    p2.join()
-    p3.join()
+    Server(10001).processes()
